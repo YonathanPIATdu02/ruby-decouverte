737c2f343ac76c24c41e28890ea90eb9f3391981
Yonathan PIAT <yonathan.piat@etudiant.univ-reims.fr>
2020-03-02 18:28:49 +0100

    push de fin de sceance

--- project/app/controllers/voitures_controller.rb
+++ project/app/controllers/voitures_controller.rb
@@ -67,7 +67,7 @@ class VoituresController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_voiture
-      @voiture = Voiture.find(params[:id])
+      @voiture = Voiture.find(params[:imat])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :marques
   resources :personnes
   resources :voitures
-  get 'voitures/immatriculation/:imat', to: 'voitures#imat'
+  get 'voitures/immatriculation/:imat', to: 'voitures#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
